d0fb0efeb913a7af13bafb5c90322c4205c6a6a4
Thunnini <yunjh1994@gmail.com>
2020-09-21 20:21:37 +0900

    Add the usage and comment for window.keplr.experimentalSuggestChain()

--- src/main.js
+++ src/main.js
@@ -13,14 +13,97 @@ window.onload = async () => {
     // If `window.cosmosJSWalletProvider` is null, Keplr extension may be not installed on browser.
     if (!window.cosmosJSWalletProvider) {
         alert("Please install keplr extension");
+    } else {
+        if (window.keplr && window.keplr.experimentalSuggestChain) {
+            try {
+                // Keplr experimentally supports the feature that suggests the chain from webpage after version v0.6.4.
+                // And, if the user approves it, it will be added to the Keplr extension.
+                // If the user rejects it or your suggested chain information doesn't include the required fields, it will throw an error.
+                // If the same chain id is already registered, it will resolve and not require the user interactions.
+                await window.keplr.experimentalSuggestChain({
+                    // Chain id of the chain.
+                    chainId: "cosmwasm-coral",
+                    // The name of the chain to be displayed to the user.
+                    chainName: "Coral",
+                    // Rpc endpoint for the chain.
+                    rpc: "https://rpc.coralnet.cosmwasm.com",
+                    // Rest endpoint for the chain.
+                    rest: "https://lcd.coralnet.cosmwasm.com",
+                    // Coin that can be used for staking purposes
+                    // But, currently, keplr doesn't have the UI that shows the tokens, so just pass it as the fee token.
+                    stakeCurrency: {
+                        // The coin denomination to be displayed to the user.
+                        coinDenom: "SHELL",
+                        // The actual coin denomination used in chain itself.
+                        coinMinimalDenom: "ushell",
+                        coinDecimals: 6,
+                        // Optinally, keplr can show the fiat value of the coin if coin gecko id is provided.
+                        // You can get id from https://api.coingecko.com/api/v3/coins/list if it is listed.
+                        // coinGeckoId: ""
+                    },
+                    // If you have the wallet webpage to be used to stake the coin and this field is provided.
+                    // Keplr will show the button to link this webpage.
+                    // walletUrlForStaking: "",
+                    // The BIP44 path.
+                    bip44: {
+                        // You can only set the coin type of BIP44.
+                        // Purpose is fixed to 44.
+                        coinType: 118,
+                    },
+                    // Bech32 configuration to show the address to user.
+                    // This field is the interface of
+                    // {
+                    //   bech32PrefixAccAddr: string;
+                    //   bech32PrefixAccPub: string;
+                    //   bech32PrefixValAddr: string;
+                    //   bech32PrefixValPub: string;
+                    //   bech32PrefixConsAddr: string;
+                    //   bech32PrefixConsPub: string;
+                    // }
+                    bech32Config: defaultBech32Config("coral"),
+                    // All coin/tokens used in this chain.
+                    currencies: [{
+                        // The coin denomination to be displayed to the user.
+                        coinDenom: "SHELL",
+                        // The actual coin denomination used in chain itself.
+                        coinMinimalDenom: "ushell",
+                        coinDecimals: 6,
+                        // Optinally, keplr can show the fiat value of the coin if coin gecko id is provided.
+                        // You can get id from https://api.coingecko.com/api/v3/coins/list if it is listed.
+                        // coinGeckoId: ""
+                    }],
+                    // All coin/tokens used as fee in this chain.
+                    feeCurrencies: [{
+                        // The coin denomination to be displayed to the user.
+                        coinDenom: "SHELL",
+                        // The actual coin denomination used in chain itself.
+                        coinMinimalDenom: "ushell",
+                        coinDecimals: 6,
+                        // Optinally, keplr can show the fiat value of the coin if coin gecko id is provided.
+                        // You can get id from https://api.coingecko.com/api/v3/coins/list if it is listed.
+                        // coinGeckoId: ""
+                    }],
+                    // The number of the coin type.
+                    // This field is only used to fetch the address from ENS.
+                    // Ideally, it is recommended to be the same with BIP44 path's coin type.
+                    // But, some early chain uses the BIP44 path as 118 which is the coin type of Cosmos Hub.
+                    // So, this is separated to support such chains.
+                    // coinType: 118,
+                });
+            } catch {
+                alert("Failed to suggest the chain");
+            }
+        } else {
+            alert("Please use the recent version of keplr extension");
+        }
     }
 
     // Initialize the gaia api with the wallet provider that is injected by Keplr extension.
     const cosmosJS = new GaiaApi({
-        chainId: "test-1",
+        chainId: "cosmwasm-coral",
         walletProvider: window.cosmosJSWalletProvider,
-        rpc: "http://127.0.0.1:26657",
-        rest: "http://127.0.0.1:1317"
+        rpc: "https://rpc.coralnet.cosmwasm.com",
+        rest: "https://lcd.coralnet.cosmwasm.com"
     }, {
         bech32Config: defaultBech32Config("coral")
     });
@@ -61,10 +144,10 @@ document.sendForm.onsubmit = () => {
     (async () => {
         // See above.
         const cosmosJS = new GaiaApi({
-            chainId: "test-1",
+            chainId: "cosmwasm-coral",
             walletProvider: window.cosmosJSWalletProvider,
-            rpc: "http://127.0.0.1:26657",
-            rest: "http://127.0.0.1:1317"
+            rpc: "https://rpc.coralnet.cosmwasm.com",
+            rest: "https://lcd.coralnet.cosmwasm.com"
         }, {
             bech32Config: defaultBech32Config("coral")
         });
