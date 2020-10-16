5c95ce0c4878f40ccf40931bc54a170af4a46fa2
Thunnini <yunjh1994@gmail.com>
2020-10-17 05:50:56 +0900

    Add some comments

--- src/main.js
+++ src/main.js
@@ -11,6 +11,8 @@ window.onload = async () => {
         if (window.keplr.experimentalSuggestChain) {
             try {
                 // Keplr v0.6.4 introduces an experimental feature that supports the feature to suggests the chain from a webpage.
+                // cosmoshub-3 is integrated to Keplr so the code should return without errors.
+                // The code below is not needed for cosmoshub-3, but may be helpful if you’re adding a custom chain.
                 // If the user approves, the chain will be added to the user's Keplr extension.
                 // If the user rejects it or the suggested chain information doesn't include the required fields, it will throw an error.
                 // If the same chain id is already registered, it will resolve and not require the user interactions.
