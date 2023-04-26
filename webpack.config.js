bfcbece8245c008693fc44b4e776708145f88bcc
delivan <wjdgurdl272@gmail.com>
2023-04-26 14:57:11 +0900

    Polish some files to remove unused code

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,20 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-    entry: {
-        main: "./src/main.js",
-    },
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname,  "dist")
-    },
-    devServer: {
-        port: 8081
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "index.html",
-            chunks: ["main"]
-        })
-    ]
+  entry: {
+    main: "./src/main.js",
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    port: 8081,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+      chunks: ["main"],
+    }),
+  ],
 };
